1eba9cbadc3953d3a1ac6dadb44d9598dc3c9b30
Eugene <bobrovice@gmail.com>
2018-02-20 12:07:12 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 module.exports = function warmup(temperature) {
-  if (temperature === -20) return -4;
-  if (temperature === 0) return 32;
-  if (temperature === 15) return 59;
-  if (temperature === 40) return 104;
+  //T(°F) = T(°C) × 9/5 + 32
+  return temperature * 9 / 5 + 32;
+  
+// if (temperature === -20) return -4;
+// if (temperature === 0) return 32;
+// if (temperature === 15) return 59;
+// if (temperature === 40) return 104;
+ 
 };
